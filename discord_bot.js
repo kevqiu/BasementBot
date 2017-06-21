ab85345be2e9d78e56759c79c37b9f3eb7892c1a
Kevin Qiu <kevinqiu96@gmail.com>
2017-06-20 23:44:40 -0400

    Initial commit

---
+++ discord_bot.js
@@ -0,0 +1,52 @@
+var Discord = require('discord.js');
+var client = new Discord.Client();
+var token = 'MzI2OTIzNTk2MzU2MTI0Njcz.DCt21g.1yxpytZ4PUoyo6LdGjz5MNgCvgs';
+
+var toEmojiCommand = /!toEmoji/;
+var redAModifier = /-a/;
+var redBModifier = /-b/;
+var spaceSplitModifier = /-space/;
+var charSplitModifier = /-char/;
+
+client.login(token);
+
+client.on('ready', () => {
+  console.log('Text to Discord Bot ready!');
+});
+
+client.on('message', message => {
+  if (toEmojiCommand.test(message.content)) {
+    var flags = getFlags(message);
+    console.log("Request sent from " + message.author.username + ": " + message)
+    message.reply(flags.redA);
+    message.reply(flags.redB);
+    message.reply(flags.spaceSplit);
+    message.reply(flags.charSplit);
+  }
+});
+
+function getFlags(msg) {
+  var flags = {
+    redA : false,
+    redB : false,
+    spaceSplit : '',
+    charSplit : ''
+  };
+
+  if(redAModifier.test(msg)) {
+    flags.redA = true;
+  }
+  if(redBModifier.test(msg)) {
+    flags.redB = true;
+  }
+  if(spaceSplitModifier.test(msg)) {
+    var msgArray = msg.toString().split(' ');
+    flags.spaceSplit = msgArray[msgArray.indexOf('-space') + 1];
+  }
+  if(charSplitModifier.test(msg)) {
+    var msgArray = msg.toString().split(' ');
+    flags.charSplit = msgArray[msgArray.indexOf('-char') + 1];
+  }
+
+  return flags;
+}
