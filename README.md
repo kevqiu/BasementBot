6cd64d670991d6b615ba8965158b78d5a45eab81
Kevin Qiu <Kevin.Qiu@visioncritical.com>
2017-06-21 15:04:25 -0400

    finished core functionality

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
 # Text To Emoji Discord Bot
 Discord chat bot that converts text into emojis.
 
-Based off of [discord.js](https://discord.js.org) package.
+Built on top of [discord.js](https://discord.js.org).
+
+## Setup
+Requirements - Node.js (version 6+)
+
+1. Clone this repo
+2. Run `npm install`
+3. Create new app under `My Apps` on [Discord Developer Console](https://discordapp.com/developers/applications/me/)
+4. Get Bot Token and add it to `config.json` (remove `.example` from file name)
+5. Add bot to server with the following URL:
+`https://discordapp.com/api/oauth2/authorize?client_id=<bot_client_id>&scope=bot`
+6. Run the bot with `node discord_bot.js`
+
+## Notes
+* Give `Manage Messages` text permissions to allow it to delete the command calls.
+* Default values are found in the `config.json` under `message`
--- discord_bot.js
+++ discord_bot.js
@@ -1,53 +1,188 @@
-var config = require('./config.json');
-var Discord = require('discord.js');
-var client = new Discord.Client();
-var token = config.discord.token;
+// Node dependencies
+const config = require('./config.json');
+const fs = require('fs');
+const Discord = require('discord.js');
+var colors = require('colors');
 
-var toEmojiCommand = /!toEmoji/;
-var redAModifier = /-a/;
-var redBModifier = /-b/;
-var spaceSplitModifier = /-space/;
-var charSplitModifier = /-char/;
+// constants
+const toEmojiCommand = '!emoji';
+const helpCommand = '!help';
+const redAModifier = '-a';
+const redBModifier = '-b';
+const spaceSplitModifier = '-s';
+const charSplitModifier = '-c';
+const numberMap = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
 
+var helpText;
+var helpLock = false;
+
+//==============================================================================
+// DISCORD BOT CLIENT
+//==============================================================================
+// Discord.js setup
+const client = new Discord.Client();
+const token = config.discord.token;
+
+// Log into bot
 client.login(token);
 
+// bot initialization
 client.on('ready', () => {
   console.log('Text to Discord Bot ready!');
+  fs.readFile(__dirname + '/help.txt', (err, data) => {
+    helpText = data.toString();
+    console.log('Help text loaded!');
+  });
 });
 
-client.on('message', message => {
-  if (toEmojiCommand.test(message.content)) {
-    var flags = getFlags(message);
-    console.log("Request sent from " + message.author.username + ": " + message)
-    message.reply(flags.redA);
-    message.reply(flags.redB);
-    message.reply(flags.spaceSplit);
-    message.reply(flags.charSplit);
+// bot message lisener
+client.on('message', msg => {
+  if (msg.content.split(' ')[0] == toEmojiCommand) { // !emoji request
+    toEmoji(msg);
+  }
+  else if (msg.content.split(' ')[0] == helpCommand) { // !help request
+    help(msg);
   }
 });
 
-function getFlags(msg) {
-  var flags = {
-    redA : false,
-    redB : false,
-    spaceSplit : '',
-    charSplit : ''
+//==============================================================================
+// MESSAGE REQUEST HANDLER METHODS
+//==============================================================================
+/*
+ * !emoji request
+*/
+function toEmoji(msg) {
+  msg.delete().catch(function(err) {
+    console.log('No delete permissions'.red);
+  });
+  var request = parseMessage(msg);
+  if (request.message && request.message.length > 0) {
+    var textChannel = msg.channel;
+    console.log(toEmojiCommand + ' request sent from ' + msg.author.username + ': ' + msg)
+    textChannel.send(msg.author.username + ':');
+    textChannel.send(emojifyMessage(request));
+  }
+}
+
+/*
+ * !help request
+*/
+function help(msg) {
+  msg.delete().catch(function(err) {
+    console.log('No delete permissions'.red);
+  });
+  if(!helpLock) {  // only allows 1 !help call in defined time frame (value in config)
+    helpLock = true; // lock the function
+    var textChannel = msg.channel;
+    console.log(helpCommand + ' request sent from ' + msg.author.username);
+    textChannel.send(helpText);
+    setTimeout(function() {helpLock = false}, config.discord.helpTimer) // unlock after timer is up
+  }
+}
+
+//==============================================================================
+// HELPER METHODS
+//==============================================================================
+/*
+ * Parses down message and returns an object which contains:
+ * message: message to be emojifieid
+ * redAFlag: flag to use red A emoji over blue
+ * redBFlag: flag to use red B emoji over blue
+ * spaceSplitFlag: string used to split white space in message
+ * charSplitFlag: string used to split characters in message
+*/
+function parseMessage(msg) {
+  var msgArray = msg.toString().split(' '); // split message to be parsed
+  var redAIndex = msgArray.indexOf(redAModifier);
+  var redBIndex = msgArray.indexOf(redBModifier);
+  var spaceModifierIndex = msgArray.indexOf(spaceSplitModifier);
+  var charModifierIndex = msgArray.indexOf(charSplitModifier);
+
+  var request = { // get defaults from config
+    message: '',
+    redAFlag: config.message.redAFlag,
+    redBFlag: config.message.redBFlag,
+    spaceSplitString: config.message.spaceSplit,
+    charSplitString: config.message.charSplit
   };
 
-  if(redAModifier.test(msg)) {
-    flags.redA = true;
+  if (redAIndex > 0) {
+    request.redAFlag = true;
   }
-  if(redBModifier.test(msg)) {
-    flags.redB = true;
+  if (redBIndex > 0) {
+    request.redBFlag = true;
   }
-  if(spaceSplitModifier.test(msg)) {
-    var msgArray = msg.toString().split(' ');
-    flags.spaceSplit = msgArray[msgArray.indexOf('-space') + 1];
+  if (spaceModifierIndex > 0) {
+    request.spaceSplitString = msgArray[spaceModifierIndex + 1];
   }
-  if(charSplitModifier.test(msg)) {
-    var msgArray = msg.toString().split(' ');
-    flags.charSplit = msgArray[msgArray.indexOf('-char') + 1];
+  if (charModifierIndex > 0) {
+    request.charSplitString = msgArray[charModifierIndex + 1];
   }
 
-  return flags;
+  // finds where message begins by determining where the last modifier lives
+  var messageStart;
+  var maxSingleModifierIndex = Math.max(redAIndex, redBIndex);
+  var maxArgumentedModifierIndex = Math.max(spaceModifierIndex, charModifierIndex)
+  if (maxArgumentedModifierIndex > maxSingleModifierIndex) { // last argument was -space <string> or -char <string>
+    messageStart = maxArgumentedModifierIndex + 2;
+  }
+  else if (maxSingleModifierIndex > maxArgumentedModifierIndex) { // last argument was -a or -b
+    messageStart = maxSingleModifierIndex + 1;
+  }
+  else { // no arguments
+    messageStart = 1;
+  }
+  request.message = msgArray.slice(messageStart).join(' ');
+
+  return request;
+}
+
+/*
+ * Turns a message into it's emojified form with Discord emojis
+ * takes flags from parseMessage and handles special cases such as :a: and :b:
+*/
+function emojifyMessage(msgObj) {
+  var emojifiedMessage = '';
+
+  if (!/\s/.test(msgObj.spaceSplitString)) { // append spaceString at the beginning of message
+    emojifiedMessage += msgObj.spaceSplitString;
+  }
+
+  var message = msgObj.message.toLowerCase().split('');
+  for (var i = 0; i < message.length; i++) {
+    var char = message[i];
+    if (/\s/.test(char)) { // white space
+      emojifiedMessage += msgObj.spaceSplitString;
+    }
+    else if (/\d/.test(char)) { // digit
+      emojifiedMessage += ':' + numberMap[parseInt(char)] + ':';
+    }
+    else if (/\w/.test(char)) { // word character
+      if (char == 'a' && msgObj.redAFlag) { // :a: case
+        emojifiedMessage += ':a:';
+      }
+      else if (char == 'b' && msgObj.redBFlag) { //:b: case
+        emojifiedMessage += ':b:';
+      }
+      else { // default letter case
+        emojifiedMessage += ':regional_indicator_' + char + ':';
+      }
+    }
+    else if(/\?/.test(char)) { // ?
+      emojifiedMessage += ':question:'
+    }
+    else if(/\!/.test(char)) { // !
+      emojifiedMessage += ':exclamation:'
+    }
+    else { // symbol and emoji case
+      emojifiedMessage += char;
+    }
+    if (!/\s/.test(message[i]) && !/\s/.test(message[i+1]) && i+1 != message.length) { // add charSplitString between letters
+      emojifiedMessage += msgObj.charSplitString;
+    }
+  }
+  if (!/\s/.test(msgObj.spaceSplitString)) { // append spaceString at the end of message
+    emojifiedMessage += msgObj.spaceSplitString;
+  }
+  return emojifiedMessage;
 }
